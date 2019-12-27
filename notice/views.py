b60d75444a6a786fbec4881d40b2709d7fa5469b
Skybluewater <Skybluewater@users.noreply.github.com>
2019-12-27 23:53:40 +0800

    通知功能的app

---
+++ notice/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from blog.models import Answer
+
+
+class CommentNoticeListView(LoginRequiredMixin, ListView):
+    context_object_name = 'notices'
+    template_name = 'notice/list.html'
+    login_url = '/login'
+
+    # 未读通知的查询集
+    def get_queryset(self):
+        return self.request.user.notifications.unread()
+
+
+class CommentNoticeUpdateView(View):
+    """更新通知状态"""
+    def get(self, request):
+        # 获取未读消息
+        notice_id = request.GET.get('notice_id')
+        # 更新单条通知
+        if notice_id:
+            article = Answer.objects.get(id=request.GET.get('article_id'))
+            request.user.notifications.get(id=notice_id).mark_as_read()
+            return redirect(article)
+        # 更新全部通知
+        else:
+            request.user.notifications.mark_all_as_read()
+            return redirect('notice:list')
