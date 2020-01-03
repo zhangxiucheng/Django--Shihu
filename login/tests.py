123cc5e3a9771e8f90d71eb4c736fde43c4a8d7e
sinofp <sinofp@tuta.io>
2020-01-03 16:47:28 +0800

    Add simple test (#3)
    * 新增一些简单基础的测试
    
    先看看能不能打开那些网页吧！
    要注册用户的话还得填验证码……怎么办？
    
    * Delete migration files
    
    不要让它们影响我的自动测试!
    
    * 删除忘了删的一个migrations文件夹和db.sqlite3
    
    * Add makemigrations to CI

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class LoginViewTests(TestCase):
+    def test_register(self):
+        """先看看能不能打开注册界面"""
+        response = self.client.get("/register/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        """先看看能不能打开登录界面"""
+        response = self.client.get("/login/")
+        self.assertEqual(response.status_code, 200)
